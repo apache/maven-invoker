23d544187a8c86d7348b5318f0506d02d5f6fccc
Sandra Parsick <sandra@parsick.dev>
2025-10-20 17:14:53 +0200

    chore: remove juni3 references
    Signed-off-by: Sandra Parsick <sandra@parsick.dev>

--- src/test/resources/test-build-should-fail/src/test/java/org/apache/maven/shared/invoker/AppTest.java
+++ src/test/resources/test-build-should-fail/src/test/java/org/apache/maven/shared/invoker/AppTest.java
@@ -19,39 +19,13 @@ package org.apache.maven.shared.invoker;
  * under the License.
  */
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    @Test
+    public void testApp() throws InterruptedException
     {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
